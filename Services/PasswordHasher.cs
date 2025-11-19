b3a22409bd86a6744ba2ca93b638b4c8dc749332
Christian Kencana <christiankencana28@gmail.com>
2025-11-19 15:19:27 +0700

    Add project files.

---
+++ Services/PasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BCrypt.Net;
+
+namespace boilerplate_desktop_dotnet.Utils
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
